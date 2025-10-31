500ef7243bc719d90711daccc54fbc5d154bb25c
Stijn <stijngeens07@gmail.com>
2025-10-31 16:12:16 +0100

    chore: added gluestack-UI components, test in app.tsx, button not working yet

--- App.tsx
+++ App.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import Home from './src/screens/Home';
 
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+import '@/global.css';
+
 export default function App() {
   return (
-    <View style={{ flex: 1 }}>
+    
+    <GluestackUIProvider mode="dark">
+      <View style={{ flex: 1 }}>
       <Home />
     </View>
+    </GluestackUIProvider>
+  
   );
 }
