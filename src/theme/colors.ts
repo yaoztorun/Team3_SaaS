ecc98ecf853e2dcce46eba35d6836e78f1f82dab
Stijn <stijngeens07@gmail.com>
2025-11-02 18:35:10 +0100

    Feature: added bottom navbar with links to dummy screens for now, changing colors for the page icons, added color pallet Chore: restructured some of the folders, to comply with standard practices

---
+++ src/theme/colors.ts
@@ -0,0 +1,16 @@
+// These match the CSS variables in global.css
+export const colors = {
+        primary: {
+                50: '#f0fdfa',   // Same as --color-primary-50
+                100: '#ccfbf1',  // Same as --color-primary-100
+                400: '#2dd4bf',  // Same as --color-primary-400
+                500: '#14b8a6',  // Same as --color-primary-500
+                600: '#0d9488',  // Same as --color-primary-600
+                900: '#134e4a',  // Same as --color-primary-900
+        },
+        neutral: {
+                400: '#9ca3af',
+                900: '#111827',
+        },
+        white: '#ffffff',
+} as const;
