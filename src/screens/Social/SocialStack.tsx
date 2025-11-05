4b79fb8d26d9d5a9ba31c10c22fc3aae5519ab92
janoaerts <jano.aerts@student.kuleuven.be>
2025-11-05 02:06:40 +0100

    created CreateParty page and made social stack for navigation
    Fronted page for CreateParty without working backend yet

---
+++ src/screens/Social/SocialStack.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SocialScreen } from './SocialScreen';
+import { CreateParty } from './CreateParty';
+
+export type SocialStackParamList = {
+    SocialMain: { initialView?: 'friends' | 'parties' } | undefined;
+    CreateParty: undefined;
+};
+
+const Stack = createNativeStackNavigator<SocialStackParamList>();
+
+export const SocialStack = () => {
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                headerShown: false,
+            }}
+        >
+            <Stack.Screen name="SocialMain" component={SocialScreen} />
+            <Stack.Screen name="CreateParty" component={CreateParty} />
+        </Stack.Navigator>
+    );
+};
