ed0cb8b9de1edb5c7f9fc9a5ed89540757b249f8
janoaerts <jano.aerts@student.kuleuven.be>
2025-11-06 19:51:49 +0100

    Made login and registartion page
    Added login and registartion fronted page and autstack to start app

---
+++ src/screens/navigation/RootStack.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { RootStackParamList } from './types';
+import { AuthStack } from './AuthStack';
+import BottomTabs from './BottomTabs';
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export const RootStack = () => {
+    return (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Auth" component={AuthStack} />
+            <Stack.Screen name="Main" component={BottomTabs} />
+        </Stack.Navigator>
+    );
+};
+
+export default RootStack;
