9cfbf23067357f5456b51ea965d521657377b970
Hugo Fache <hugofache@gmail.com>
2025-11-11 13:22:46 +0100

    Google login works, still need redirect. Still need email registering, and test email login

--- App.tsx
+++ App.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { GluestackUIProvider } from '@/src/components/ui/gluestack-ui-provider';
 import { RootStack } from './src/screens/navigation/RootStack';
+import { AuthStack } from './src/screens/navigation/AuthStack';
+import { useAuth } from './src/hooks/useAuth';
 import '@/global.css';
 
 export default function App() {
+  const { user, loading } = useAuth();
+
+  if(loading) {
+    return <></>; // or a loading spinner
+  }
+
   return (
     <GluestackUIProvider mode="light">
       <NavigationContainer>
-        <RootStack />
+        {user ? <RootStack /> : <AuthStack />}
       </NavigationContainer>
     </GluestackUIProvider>
   );
