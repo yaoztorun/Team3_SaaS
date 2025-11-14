2ed87d3c3a7b2dac34d12fe7434a49061b6f95ee
laurens <laurens.pirard@student.kuleuven.be>
2025-11-14 14:10:39 +0100

    backend Best bars

---
+++ src/types/location.ts
@@ -0,0 +1,12 @@
+export type Location = {
+    id: string;
+    created_at?: string;
+    name: string;
+    street_name?: string;
+    street_nr?: string;
+    city?: string;
+    country?: string;
+    description?: string;
+    image_url?: string;
+    rating?: number;
+};
