82746cc5f7d0a591fa66ac02f5f318e0ed434f43
Hugo Fache <hugofache@gmail.com>
2025-11-26 20:54:14 +0100

    Implemented basic user settings

--- src/types/profile.ts
+++ src/types/profile.ts
@@ -1,7 +1,10 @@
+import { UserSettings } from './settings';
+
 export type Profile = {
     id: string;
     created_at: string;
     full_name: string | null;
     email: string | null;
     avatar_url: string | null;
+    settings: UserSettings | null;
 };
