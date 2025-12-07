db7877eed41251f196ca7d97db0095c2964c206d
Hugo Fache <hugofache@gmail.com>
2025-12-07 11:55:19 +0100

    Bugfix: toggle border padding

--- src/components/global/ToggleSwitch.tsx
+++ src/components/global/ToggleSwitch.tsx
@@ -66,7 +66,7 @@ export const ToggleSwitch: React.FC<ToggleSwitchProps> = ({
     return (
         <Box 
             className="rounded-xl overflow-hidden"
-            style={{ flexDirection: 'row', borderWidth: 2, borderColor: '#e5e7eb', backgroundColor: '#ffffff', padding: 4 }}
+            style={{ flexDirection: 'row', borderWidth: 2, borderColor: '#e5e7eb', backgroundColor: '#ffffff', padding: 2 }}
         >
             <Pressable 
                 className="flex-1 py-2 px-3 justify-center rounded-lg"
